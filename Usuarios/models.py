ce4ca51f0a96a42a5d6d4ebabf80da2744909cdb
egresados <rclavijo@utp.edu.co>
2019-07-19 00:48:46 +0000

    server

--- Usuarios/models.py
+++ Usuarios/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.core.validators import MinLengthValidator
 from django.dispatch import receiver
 from django.db.models.signals import post_save
- 
+
 def custom_uptload_to(instance, filename):
     old_instance = Profile.objects.get(pk=instance.pk)
     old_instance.avatar.delete()
@@ -16,12 +16,12 @@ class User(AbstractUser):
     ('Masculino', 'Masculino'),
     ('Femenino', 'Femenino'),
     ('Indefinido', 'Indefinido'),
-  ] 
+  ]
   is_Admon = models.BooleanField(default=False)
   is_Egresado = models.BooleanField(default=False)
   document = models.CharField(verbose_name="Documento", unique = True, max_length = 30)
   address = models.CharField(verbose_name="Dirección", max_length=20, validators=[MinLengthValidator(7)], null= True, blank= True)
-  city = models.CharField(verbose_name="Ciudad", max_length=20, validators=[MinLengthValidator(7)], null= True, blank= True)
+  city = models.CharField(verbose_name="Ciudad", max_length=20, validators=[MinLengthValidator(4)], null= True, blank= True)
   gender = models.CharField(verbose_name="Genero", null= True, blank= True, max_length = 30, choices=GENRE_CHOICES)
   phone = models.IntegerField(verbose_name="Telefono", null= True, blank= True)
   validate = models.BooleanField(default=True)
@@ -34,24 +34,24 @@ class Interests(models.Model):
 
 	def __str__(self):
 		return self.name
-    
+
 
 class Egresado(models.Model):
   VALIDATE_CHOICES = [
     ('validado', 'Validado'),
     ('Espera', 'Espera'),
     ('Rechazado', 'Rechazado'),
-  ] 
+  ]
   user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
-  active = models.BooleanField(default=True)   
-  country = models.TextField(verbose_name="Pais", null= True, blank= True)  
-  datebirth = models.DateField(verbose_name="Fecha De Nacimiento", null= True, blank= True)  
+  active = models.BooleanField(default=True)
+  country = models.TextField(verbose_name="Pais", null= True, blank= True)
+  datebirth = models.DateField(verbose_name="Fecha De Nacimiento", null= True, blank= True)
   created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
   updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
-    
+
   def __str__(self):
     return self.user.first_name
-    
+
 class Profile(models.Model):
     User = models.OneToOneField(User , on_delete = models.CASCADE)
     avatar = models.ImageField(upload_to=custom_uptload_to, null = True, blank=True)
@@ -60,15 +60,14 @@ class Profile(models.Model):
     link = models.URLField(max_length=200,null = True, blank=True)
 
 class EgresadoConsulta(models.Model):
-  active = models.BooleanField(default=True)   
+  active = models.BooleanField(default=True)
   document = models.CharField(verbose_name="Documento", unique = True, max_length = 30)
   programa = models.TextField(verbose_name="Programa", null= True, blank= True)
 
-    
+
   def __str__(self):
     return self.document
 
 
 
 
-      
--- Usuarios/templates/registration/Adminpassword.tpl
+++ Usuarios/templates/registration/Adminpassword.tpl
@@ -5,13 +5,13 @@ Creación Cuenta {{ user.username }}
 {% endblock %}
 
 {% block body %}
-Tu Cuenta Ha sido Creada para poder Utilizarla, Ingresa tu correo en el siguiente link 
+Tu Cuenta Ha sido Creada para poder Utilizarla, Ingresa tu correo en el siguiente link
 Para crear una nueva contraseña
 {% endblock %}
 
 {% block html %}
-<p>Tu Cuenta Ha sido Creada para poder Utilizarla, Ingresa tu correo en el siguiente link 
+<p>Tu Cuenta Ha sido Creada para poder Utilizarla, Ingresa tu correo en el siguiente link
 Para crear una nueva contraseña</p>
-<a href = "http://127.0.0.1:8000/cuentas/password_reset"> click aqui </a>
+<a href = "http://egresados.pythonanywhere.com/cuentas/password_reset"> click aqui </a>
 tu nombre de usuario para inicio de sesión es: {{user.username}}
 {% endblock %}
